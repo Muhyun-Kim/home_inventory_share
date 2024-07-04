93e39dba6108fa4c84c0aa218d20a621a07fb61a
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2024-07-05 00:31:37 +0900

    add inventory

--- lib/views/inventory/create_inventory/create_inventory_page.dart
+++ lib/views/inventory/create_inventory/create_inventory_page.dart
@@ -10,11 +10,46 @@ class CreateInventoryPage extends ConsumerStatefulWidget {
 }
 
 class _CreateInventoryPageState extends ConsumerState<CreateInventoryPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _inventoryController = TextEditingController();
+  final _quantityController = TextEditingController();
+  IconData? _selectIcon;
+
+  @override
+  void dispose() {
+    _inventoryController.dispose();
+    _quantityController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('create inventory'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.00),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _inventoryController,
+                decoration: InputDecoration(
+                  labelText: 'inventory',
+                ),
+              ),
+              TextFormField(
+                controller: _quantityController,
+                decoration: InputDecoration(
+                  labelText: 'quantity',
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
