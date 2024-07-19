61333ab861d6fd7098d8da1ec7f357c3bbe46689
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2024-07-19 14:10:23 +0900

    read inventory list

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:home_inventory_share/controllers/auth_controller.dart';
+import 'package:home_inventory_share/providers/inventory_provider.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
@@ -9,6 +10,7 @@ class HomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final authController = ref.read(authControllerProvider);
+    final inventoryList = ref.watch(inventoryProvider);
 
     Future<void> logout() async {
       await authController.logout();
@@ -19,27 +21,46 @@ class HomePage extends ConsumerWidget {
         title: const Text('Home Page'),
       ),
       drawer: Drawer(
-        child: ListView(children: <Widget>[
-          const DrawerHeader(
-            child: Center(
-              child: Text('Home Inventory'),
-            ),
-          ),
-          Column(
-            children: [
-              ListTile(
-                title: const Text(
-                  'ログアウト',
-                  style: TextStyle(
-                    color: Colors.red,
-                  ),
-                ),
-                onTap: logout,
+        child: ListView(
+          children: <Widget>[
+            const DrawerHeader(
+              child: Center(
+                child: Text('Home Inventory'),
               ),
-            ],
-          )
-        ]),
+            ),
+            Column(
+              children: [
+                ListTile(
+                  title: const Text(
+                    'ログアウト',
+                    style: TextStyle(
+                      color: Colors.red,
+                    ),
+                  ),
+                  onTap: logout,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
+      body: inventoryList.isEmpty
+          ? Center(child: Text("生活用品を追加しよう"))
+          : ListView.builder(
+              itemCount: inventoryList.length,
+              itemBuilder: (context, index) {
+                final inventory = inventoryList[index];
+                return ListTile(
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(inventory.inventoryName),
+                      Text('${inventory.inventoryQuantity}'),
+                    ],
+                  ),
+                );
+              },
+            ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => context.push('/inventory/create'),
         child: const Icon(
