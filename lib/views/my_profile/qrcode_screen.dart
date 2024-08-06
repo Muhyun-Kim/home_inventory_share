507b399a8c3255ac3352e108093ca31c29a50801
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2024-08-06 22:59:06 +0900

    add qrcode

--- lib/views/my_profile/qrcode_screen.dart
+++ lib/views/my_profile/qrcode_screen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:home_inventory_share/providers/auth_provider.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
-class QrcodeScreen extends StatelessWidget {
+class QrcodeScreen extends ConsumerWidget {
   const QrcodeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authState = ref.watch(authStateProvider);
     return Center(
-      child: Text("QRcode"),
+      child: QrImageView(
+        data: authState.user!.userId,
+        version: QrVersions.auto,
+        size: 300.0,
+      ),
     );
   }
 }
